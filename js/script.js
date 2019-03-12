0204faa47bb8fd72896f9e1d99191ee5f74e6d28
kirillcheck <32092781+kirillcheck@users.noreply.github.com>
2019-03-13 00:23:37 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,28 +3,27 @@ window.addEventListener('load', function () {
     let h1 = document.querySelector('h1');
 
     function getTimer() {
-        let time = new Date();
-        let Hours = time.getHours();
-        let Minutes = time.getMinutes();
-        let Seconds = time.getSeconds();
+        let time = new Date(),
+            Hours = time.getHours(),
+            Minutes = time.getMinutes(),
+            Seconds = time.getSeconds();
+        if (Hours < 10) {
+            Hours = '0' + Hours;
+        }
         if (Seconds < 10) {
             Seconds = '0' + Seconds;
         }
         if (Minutes < 10) {
             Minutes = '0' + Seconds;
         }
-        if (Hours < 10) {
-            Hours = '0' + Seconds;
-        }
 
         h1.textContent = Hours + ':' + Minutes + ':' + Seconds;
         setTimeout(getTimer, 1000);
+        console.log(Hours);
     }
     getTimer();
 
 });
-
-
 
 
 
